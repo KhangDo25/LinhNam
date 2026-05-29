94f7ff48c3ff8a38439415bfa045457b81ab2001
VGO-cell <khangdo0929@gmail.com>
2026-05-29 21:13:22 +0700

    Updates

--- _next/static/ZHJyTwXxna1o1PG7FesD0/_buildManifest.js
+++ _next/static/ZHJyTwXxna1o1PG7FesD0/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/LinhNam//_next/:path+",
+        "destination": "/LinhNam/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
