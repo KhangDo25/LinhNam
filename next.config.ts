6d870973f956af813ec576cf26842eef23642a66
VGO-cell <khangdo0929@gmail.com>
2026-05-29 20:54:03 +0700

    config: them basePath cho github pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/LinhNam',
   images: {
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
