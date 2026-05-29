4942847ff55ecc2b6f9a19895dc54be2f9085b56
VGO-cell <khangdo0929@gmail.com>
2026-05-29 20:48:51 +0700

    deploy: xuat ban giao dien su thi linh nam

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.100.182"],
+  output: 'export', 
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        pathname: "/**",
-      },
-    ],
+    unoptimized: true, 
   },
 };
 
